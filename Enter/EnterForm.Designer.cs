f567587c55d03173eaf3a78bd2f9aa00a9f9fa7b
Артём <artemkunitsyn@gmail.com>
2016-08-09 15:14:58 +0400

    Completed database interface

--- Enter/EnterForm.Designer.cs
+++ Enter/EnterForm.Designer.cs
@@ -33,26 +33,25 @@
             this.btnLogin = new System.Windows.Forms.Button();
             this.lbLogin = new System.Windows.Forms.Label();
             this.lbPass = new System.Windows.Forms.Label();
-            this.btnAdminLog = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tbLogin
             // 
-            this.tbLogin.Location = new System.Drawing.Point(108, 29);
+            this.tbLogin.Location = new System.Drawing.Point(104, 22);
             this.tbLogin.Name = "tbLogin";
             this.tbLogin.Size = new System.Drawing.Size(152, 20);
             this.tbLogin.TabIndex = 3;
             // 
             // tbPassword
             // 
-            this.tbPassword.Location = new System.Drawing.Point(108, 55);
+            this.tbPassword.Location = new System.Drawing.Point(104, 48);
             this.tbPassword.Name = "tbPassword";
             this.tbPassword.Size = new System.Drawing.Size(152, 20);
             this.tbPassword.TabIndex = 4;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(185, 81);
+            this.btnLogin.Location = new System.Drawing.Point(181, 74);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(75, 23);
             this.btnLogin.TabIndex = 5;
@@ -63,7 +62,7 @@
             // lbLogin
             // 
             this.lbLogin.AutoSize = true;
-            this.lbLogin.Location = new System.Drawing.Point(28, 32);
+            this.lbLogin.Location = new System.Drawing.Point(24, 25);
             this.lbLogin.Name = "lbLogin";
             this.lbLogin.Size = new System.Drawing.Size(41, 13);
             this.lbLogin.TabIndex = 6;
@@ -72,33 +71,24 @@
             // lbPass
             // 
             this.lbPass.AutoSize = true;
-            this.lbPass.Location = new System.Drawing.Point(28, 58);
+            this.lbPass.Location = new System.Drawing.Point(24, 51);
             this.lbPass.Name = "lbPass";
             this.lbPass.Size = new System.Drawing.Size(48, 13);
             this.lbPass.TabIndex = 7;
             this.lbPass.Text = "Пароль:";
             // 
-            // btnAdminLog
-            // 
-            this.btnAdminLog.Location = new System.Drawing.Point(12, 104);
-            this.btnAdminLog.Name = "btnAdminLog";
-            this.btnAdminLog.Size = new System.Drawing.Size(129, 23);
-            this.btnAdminLog.TabIndex = 1;
-            this.btnAdminLog.Text = "Суперпользователь";
-            this.btnAdminLog.UseVisualStyleBackColor = true;
-            this.btnAdminLog.Click += new System.EventHandler(this.btnAdminLog_Click);
-            // 
             // EnterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(286, 139);
+            this.ClientSize = new System.Drawing.Size(284, 113);
             this.Controls.Add(this.lbPass);
             this.Controls.Add(this.lbLogin);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.tbLogin);
-            this.Controls.Add(this.btnAdminLog);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "EnterForm";
             this.Text = "Окно логина";
             this.ResumeLayout(false);
@@ -112,7 +102,6 @@
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label lbLogin;
         private System.Windows.Forms.Label lbPass;
-        private System.Windows.Forms.Button btnAdminLog;
     }
 }
 
